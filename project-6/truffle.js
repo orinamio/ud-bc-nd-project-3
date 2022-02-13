a522d270246716083f389c51e9d6e767f05c9ed8
Orinami <orinamiolatunji@gmail.com>
2022-02-13 22:08:08 +0400

    deploy contract to rinkeby

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,27 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const rinkebyProvider = function () {
+	return new HDWalletProvider(
+		process.env.RINKEBY_MNEMONIC,
+		`https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+	);
+};
+
 module.exports = {
 	networks: {
 		development: {
 			host: "127.0.0.1",
 			port: 8545,
 			network_id: "*", // Match any network id
+		},
+		rinkeby: {
+			provider: rinkebyProvider,
+			network_id: 4,
+			confirmations: 1,
+			skipDryRun: true,
 		},
 	},
 	mocha: {
