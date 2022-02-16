3ed88a665d721c28abefb8ff4a0ec8fd4c321b01
Orinami <orinamiolatunji@gmail.com>
2022-02-17 02:24:32 +0400

    refactor: split SupplyChain contract

--- project-6/migrations/2_deploy_contracts.js
+++ project-6/migrations/2_deploy_contracts.js
@@ -4,11 +4,14 @@ var DistributorRole = artifacts.require("./DistributorRole.sol");
 var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
+var Coffee = artifacts.require("./Coffee.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(FarmerRole);
-  deployer.deploy(DistributorRole);
-  deployer.deploy(RetailerRole);
-  deployer.deploy(ConsumerRole);
+module.exports = function (deployer) {
+	deployer.deploy(FarmerRole);
+	deployer.deploy(DistributorRole);
+	deployer.deploy(RetailerRole);
+	deployer.deploy(ConsumerRole);
+  deployer.deploy(Coffee);
+  deployer.link(Coffee, SupplyChain);
   deployer.deploy(SupplyChain);
 };
--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -117,7 +117,7 @@ contract("SupplyChain", function (accounts) {
 		});
 
 		// Mark an item as Processed by calling function processtItem()
-		await supplyChain.processItem(upc);
+		await supplyChain.processItem(upc, { from: originFarmerID });
 
 		// Retrieve the just now saved item from blockchain by calling function fetchItem()
 		const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc);
@@ -142,7 +142,7 @@ contract("SupplyChain", function (accounts) {
 		});
 
 		// Mark an item as Packed by calling function packItem()
-		await supplyChain.packItem(upc);
+		await supplyChain.packItem(upc, { from: originFarmerID });
 
 		// Retrieve the just now saved item from blockchain by calling function fetchItem()
 		const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc);
@@ -167,7 +167,7 @@ contract("SupplyChain", function (accounts) {
 		});
 
 		// Mark an item as ForSale by calling function sellItem()
-		await supplyChain.sellItem(upc, productPrice);
+		await supplyChain.sellItem(upc, productPrice, { from: originFarmerID });
 
 		// Retrieve the just now saved item from blockchain by calling function fetchItem()
 		const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc);
@@ -231,7 +231,7 @@ contract("SupplyChain", function (accounts) {
 		});
 
 		// Mark an item as Sold by calling function shipItem()
-		await supplyChain.shipItem(upc);
+		await supplyChain.shipItem(upc, { from: distributorID });
 
 		// Retrieve the just now saved item from blockchain by calling function fetchItem()
 		const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc);
